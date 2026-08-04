b788abb58474b23e62ddf97bcef1347dbc761e64
VENU MADHAV REDDY DHANIREDDY <venumadhavdhanireddy@gmail.com>
2026-08-04 08:31:00 +0530

    Time: 5 ms (72.33%) | Memory: 19.2 MB (54.10%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,19 @@
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         s = str(x)
+#         return s == s[::-1]
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        original = x
+        reverse = 0
+
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x //= 10
+
+        return original == reverse
