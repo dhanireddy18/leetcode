05e3544838c7ce10b760de764df44deaa9a73f8a
VENU MADHAV REDDY DHANIREDDY <venumadhavdhanireddy@gmail.com>
2026-08-07 15:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (55.71%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -5,27 +5,38 @@ class Solution:
         # for i in str(nums):
         #     narr.append(int(i))
         # return narr
-        if digits[-1]<9:
-             digits[-1]+=1
-             return digits
-        else:
-            carry=1
-            for i in range(len(digits)-1,-1,-1):
-                if digits[i]==9 and carry==1:
-                    digits[i]=0
-                    carry=1
-                else:
-                    s=digits[i]+carry
-                    if s>9:
-                        digits[i]=0
-                        carry=1
-                    else:
-                        digits[i]=s
-                        carry=0
-        if carry==1:
-            digits.insert(0,1)
-        return digits
-                    
+
+
+        # if digits[-1]<9:
+        #      digits[-1]+=1
+        #      return digits
+        # else:
+        #     carry=1
+        #     for i in range(len(digits)-1,-1,-1):
+        #         if digits[i]==9 and carry==1:
+        #             digits[i]=0
+        #             carry=1
+        #         else:
+        #             s=digits[i]+carry
+        #             if s>9:
+        #                 digits[i]=0
+        #                 carry=1
+        #             else:
+        #                 digits[i]=s
+        #                 carry=0
+        # if carry==1:
+        #     digits.insert(0,1)
+        # return digits
+
+
+        n = len(digits)
+        for i in range(n-1,-1,-1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            else:
+                digits[i] = 0
+        return [1]+digits                
 
 
 
